555fc6df01c9826f349cb83a60c0c8c79043f540
TobiasPettersson <pettersson.tobias84@gmail.com>
2017-11-07 21:24:14 +0100

    Implemented JAX-RS

---
+++ src/main/java/JAXRSConfigurations.java
@@ -0,0 +1,10 @@
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+@ApplicationPath("")
+public class JAXRSConfigurations extends Application {
+
+    // nothing to configuree
+
+}
